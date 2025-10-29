3b8b04de7fa09d1096ee6651b3f02cb51fcd0737
Hilmi-Sabitul-Azmi <hilmi170717@gmail.com>
2025-10-29 21:55:36 +0700

    tugas 5

---
+++ Tugas5_301250029_Hilmi-Sabitul_Azmi.py
@@ -0,0 +1,17 @@
+for i in range(5,0,-1):         #pola angka menurun
+    for j in range(1,i+1):
+        print(j,end='')
+        
+    print()
+
+for i in range(1,6):            #pola segitiga kiri
+    for j in range(i):
+        print('*',end='')
+
+    print()
+
+for i in range(5):              #pola persegi
+    for j in range(5):
+        print('*',end='')
+
+    print()
